a83b96fa211341710f616c438ac2c01c27126089
Banovsha2003 <78psrjd@code.edu.az>
2023-04-13 04:34:02 +0400

    Add project files.

---
+++ Generics hw/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Generics_hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////task:5
+            //GamePlayer<Point> gamer = new GamePlayer<Point>();
+        }
+    }
+    ////task:1
+    //class GamePlayer<T>
+    //{
+    //    public List<T> GamePlayers;
+
+    //    public void AddPlayer(T game)
+    //    {
+    //        GamePlayers.Add(game);
+    //    }
+    //}
+
+
+    ////task:2
+    //interface ICharacter<T>
+    //{
+    //    void Attacking<T>(T gamer);
+    //    void TakingDamage<T>(T enemy);
+    //    void Healing<T>(T gamer);
+
+    //}
+
+
+    ////task:3
+    //class Inventory<T>
+    //{
+    //    public List<T> Elements { get; set; }
+
+    //    public void Add(T item)
+    //    {
+    //        Elements.Add(item);
+    //    }
+    //    public void Remove(T item)
+    //    {
+    //        Elements.Remove(item);
+    //    }
+    //    public void Sort()
+    //    {
+    //        Elements.Sort();
+    //    }
+    //}
+
+
+    ////task:4
+
+
+    ////task:5
+    //class GamePlayer<T>
+    //{
+    //    public List<T> Players { get; set; }
+    //    public void Add(T player)
+    //    {
+    //        Players.Add(player);
+    //    }
+    //    public void Remove(T player)
+    //    {
+    //        Players.Remove(player);
+    //    }
+    //    public void Sort()
+    //    {
+    //        Players.Sort();
+    //    }
+    //}
+
+    //class Player
+    //{
+    //}
+
+
+    ////task:6
+    //public class GameObject
+    //{
+    //    public string Name { get; set; }
+    //    public int Level { get; set; }
+    //    public int Score { get; set; }
+
+    //    public void Names(string name)
+    //    {
+    //        Name = name;
+    //    }
+    //    public void LevelS(int level)
+    //    {
+    //        Level = level;
+    //    }
+    //    public void ScoreS(int score)
+    //    {
+    //        Score = score;
+    //    }
+    //}
+
+
+
+}
